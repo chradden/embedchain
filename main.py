eac480d8045289f94fa8cd0f6843b4956bd24ec2
Christian Radden <102736612+chradden@users.noreply.github.com>
2023-06-29 17:01:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ if st.button("Build Model"):
     try:
       YTbot.add("youtube_video", site)
       st.session_state['crawling'] = True
+    except Exception as e:
+          st.error(f"An error occurred: {e}")
+          st.error('Oops, the GPT response resulted in an error :( Please try again with a different question.')
 if site and ("crawling" in state):
       st.header("Ask your data")
       user_q = st.text_input("Enter your questions here")
