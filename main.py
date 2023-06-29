56286dabef7932eac8562619008728c0c429f830
Christian Radden <102736612+chradden@users.noreply.github.com>
2023-06-29 17:33:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,26 +12,18 @@ st.set_page_config(layout="centered", page_title="YT QnA")
 st.header("YT QnA Bot 🤖")
 state = st.session_state
 site = st.text_input("Enter your URL here")
-if st.button("Build Model"):
-  if site is None:
-    st.info(f"""Enter YT to Build QnA Bot""")
-  elif site:
+user_q = st.text_input("Enter your questions here")
+if st.button("Get response"):
+  if site is not None and user_q is not None:
     st.write(str(site) + " starting to crawl..")
     try:
       YTbot.add("youtube_video", site)
       st.session_state['crawling'] = True
+      
+      with st.spinner("Model is working on it..."):
+        result = YTbot.query(str(user_q))
+        st.subheader('Your response:')
+        st.write(result)
     except Exception as e:
               st.error(f"An error occurred: {e}")
               st.error('Oops, crawling resulted in an error :( Please try again with a different URL.')
-if site and ("crawling" in state):
-      st.header("Ask your data")
-      user_q = st.text_input("Enter your questions here")
-      if st.button("Get Response"):
-        try:
-          with st.spinner("Model is working on it..."):
-            result = YTbot.query(str(user_q))
-            st.subheader('Your response:')
-            st.write(result)
-        except Exception as e:
-          st.error(f"An error occurred: {e}")
-          st.error('Oops, the GPT response resulted in an error :( Please try again with a different question.')    
